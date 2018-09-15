4e2ee760ce92f9b913e675571bc62959a313eeb5
Tien Mai <maitien2004@xuongrong.info>
2018-09-15 23:07:49 +0700

    - Update trustAnchor.ts

--- project/server/models/trustAnchor.ts
+++ project/server/models/trustAnchor.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 const trustAnchorSchema = new mongoose.Schema({
   trustAnchorName: String,
   trustAnchorDID: String,
-  trustAnchorWallet: Number,
+  trustAnchorWallet: String,
   stewardTrustAnchorKey: String,
   trustAnchorStewardDid: String,
   trustAnchorStewardKey: String
